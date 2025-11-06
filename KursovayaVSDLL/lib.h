307cd768a68d81f88139835e25e936961fdcbc76
Zhenya Nikiforov <12A92@mail.ru>
2025-11-06 13:06:49 +0500

    Рефакторинг классов трансп. средств

--- KursovayaVSDLL/lib.h
+++ KursovayaVSDLL/lib.h
@@ -7,64 +7,63 @@
 
 #include <iostream>
 
-class KURSOVAYAVSDLL_API camel {
+class KURSOVAYAVSDLL_API camel {//класс верблюда
 public:
-	camel(double speed = 10, double timeBeforeRest = 30, double firstRest = 5, double secondRest = 8 );
-	virtual double calcTime(double distantion);
-protected:
+	camel(double speed = 10, double timeBeforeRest = 30, double firstRest = 5, double secondRest = 8 );//конструктор
+	virtual double calcTime(double distantion);//будем переопределять в наследнике
+protected://наследуемые поля
 	double speed = 0;
 	double timeBeforeRest = 0;
 	double firstRest = 0;
 	double secondRest = 0;
 };
 
-class KURSOVAYAVSDLL_API speedCamel:public camel {
+class KURSOVAYAVSDLL_API speedCamel:public camel {//наследуем скоростного верблюда от обычного
 public:
 	speedCamel(double speed = 40, double timeBeforeRest = 10, double firstRest = 5, double secondRest = 6.5, double nextRest = 8) : camel(speed, timeBeforeRest, firstRest, secondRest){};
-	double calcTime (double distantion) override;
+	double calcTime (double distantion) override;//-переопределяем расчёт времени
 protected:
 	double nextRest = 8;
 };
 
-class KURSOVAYAVSDLL_API kentauros {
+class KURSOVAYAVSDLL_API kentauros {//-класс кентавра
 public:
 	kentauros(double speed = 15, double timeBeforeRest = 8, double firstRest = 2);
-	virtual double calcTime(double distantion);
+	virtual double calcTime(double distantion);//-будем переопределять в наследнике
 protected:
 	double speed = 0;
 	double timeBeforeRest = 0;
 	double firstRest = 0;
 };
 
-class KURSOVAYAVSDLL_API boots:public kentauros {
+class KURSOVAYAVSDLL_API boots:public kentauros {//наследуем ботинки от кентавра
 public:
 	boots(double speed = 6, double timeBeforeRest = 60, double firstRest = 10, double secondRest = 5):kentauros(speed, timeBeforeRest, firstRest) {};
-	void showBoots();
-	double calcTime(double distantion) override;
+	double calcTime(double distantion) override;//переопределяем расчёт времени
 protected:
 	double secondRest = 5;
 };
 
-class KURSOVAYAVSDLL_API eagle {
+class KURSOVAYAVSDLL_API eagle {//класс орла;
 public:
 	eagle(double speed = 8) {};
-	virtual double calcTime(double distantion);
+	virtual double calcTime(double distantion);//будем переопределять в ковре и метле;
 protected:
 	double speed = 8;
 };
 
-class KURSOVAYAVSDLL_API flyingCarpet :public eagle {
+class KURSOVAYAVSDLL_API flyingCarpet :public eagle {//летающий ковёр от орла;
 public:
 	flyingCarpet(double speed = 10) :eagle(speed) {};
-	double calcTime(double distantion) override;
+	double calcTime(double distantion) override;//переопределяем расчёт времени;
 protected:
 	double speed = 10;
 };
 
-class KURSOVAYAVSDLL_API broom :public eagle {
+class KURSOVAYAVSDLL_API broom :public eagle {//метла от орла;
 public:
 	broom(double speed = 20) :eagle(speed) {};
-	double calcTime(double distantion) override;
+	double calcTime(double distantion) override;//переопределяем расчёт времени;
 protected:
 	double speed = 20;
 };
